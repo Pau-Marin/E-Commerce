d0e358893d6008cf929e5c82cbd933409595dac3
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-12-02 18:31:36 +0100

    Product page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import Home from "./pages/Home"
+import Product from "./pages/Product"
 import ProductList from "./pages/ProductList"
 
 export default function App() {
-    return <ProductList />
+    return <Product />
 }
