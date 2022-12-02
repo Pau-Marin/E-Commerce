5f3eb6966958121a72a0337ca626538d1ea2c55b
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-12-02 19:13:33 +0100

    Login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,8 @@ import Home from "./pages/Home"
 import Product from "./pages/Product"
 import ProductList from "./pages/ProductList"
 import Register from "./pages/Register"
+import Login from "./pages/Login"
 
 export default function App() {
-    return <Register />
+    return <Login />
 }
