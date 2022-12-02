ef36f86f3d1c0e1acbc8306aca2463f2cc54c44c
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2022-12-02 13:10:53 +0100

    Added keys to avoid warnings

--- frontend/src/components/Categories.jsx
+++ frontend/src/components/Categories.jsx
@@ -14,7 +14,7 @@ export default function Categories() {
     return (
         <Container>
             {categories.map((item) => (
-                <CategoryItem item={item} />
+                <CategoryItem item={item} key={item.id} />
             ))}
         </Container>
     )
