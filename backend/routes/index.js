d1e466f19c956873ed0ebce6759b536d103b96d7
Pau-Marin <ritsu95@icloud.com>
2022-12-26 13:21:49 +0100

    Order Routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,12 +2,14 @@ const router = require("express").Router()
 const authRoute = require("./auth")
 const userRoute = require("./user")
 const cartRoute = require("./cart")
+const orderRoute = require("./order")
 const productRoute = require("./product")
 
 // Routes
 router.use("/api/auth", authRoute)
 router.use("/api/users", userRoute)
 router.use("/api/carts", cartRoute)
+router.use("/api/orders", orderRoute)
 router.use("/api/products", productRoute)
 
 module.exports = router
